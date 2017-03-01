9954b4ad0748de6a62446a09ff657fc9793c980f
jeffreysdempsey <jeffreysdempsey@gmail.com>
2017-02-28 18:30:07 -0600

    fixed radio buttons

--- app/src/main/java/com/example/jeffdempsey/nativeapp/MainActivity.java
+++ app/src/main/java/com/example/jeffdempsey/nativeapp/MainActivity.java
@@ -9,27 +9,26 @@ import android.widget.Toast;
 import android.view.View;
 
 public class MainActivity extends Activity {
-
-    final RadioGroup group1 = (RadioGroup) findViewById(R.id.orientation);
-    group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(RadioGroup.group, int checkedId) {
-            switch (checkedId) {
-                case R.id.horizontal:
-                    group.setOrientation(LinearLayout.HORIZONTAL);
-                    break;
-                case R.id.vertical:
-                    group.setOrientation(LinearLayout.VERTICAL);
-                    break;
-            }
-        }
-    });
-
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        final RadioGroup group1 = (RadioGroup) findViewById(R.id.orientation);
+        group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.horizontal:
+                        group.setOrientation(LinearLayout.HORIZONTAL);
+                        break;
+                    case R.id.vertical:
+                        group.setOrientation(LinearLayout.VERTICAL);
+                        break;
+                    }
+                }
+            });
+        }
 
     public void onClick (View view) {
         EditText input = (EditText) findViewById(R.id.main_input);
